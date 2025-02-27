fc1787f8aaa1b07d6150467e85ed39e4a7b5f7bf
serchg0george <serchg0george@gmail.com>
2025-02-27 19:52:08 +0200

    Deleted address, mail entities and all layers linked to it

--- src/main/java/com/api/management/user/dto/employee/MailToEmployeeDto.java
+++
@@ -1,8 +0,0 @@
-package com.api.management.user.dto.employee;
-
-import jakarta.validation.constraints.NotNull;
-
-
-public record MailToEmployeeDto(@NotNull Long mailId,
-                                @NotNull Long peopleId) {
-}
--- src/main/java/com/api/management/user/repository/MailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.management.user.repository;
-
-import com.api.management.user.entity.MailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MailRepository extends JpaRepository<MailEntity, Long> {
-}
