1a583af11e414d64b0b55ecbf8efe7f8aa30f2f3
serchg0george <serchg0george@gmail.com>
2025-02-27 22:08:52 +0200

    Renamed repo

--- src/main/java/com/api/management/user/repository/TimeSheetRepository.java
+++ src/main/java/com/api/management/user/repository/TimeSheetRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimeTrackingRepository extends JpaRepository<TimeSheetEntity, Long> {
+public interface TimeSheetRepository extends JpaRepository<TimeSheetEntity, Long> {
 }
