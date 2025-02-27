e9f97f9665a2023a9c1ce8d72eb168d17762e1df
serchg0george <serchg0george@gmail.com>
2025-02-27 18:44:23 +0200

    Added @Repository annotation

--- src/main/java/com/api/management/user/repository/UserRepository.java
+++ src/main/java/com/api/management/user/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.api.management.user.repository;
 
 import com.api.management.user.entity.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByEmail(String email);
-
 }
