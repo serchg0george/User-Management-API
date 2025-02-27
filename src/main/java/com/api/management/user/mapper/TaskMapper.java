714bb3510e7d9e7fbb42ecb5612ce052571002f0
serchg0george <serchg0george@gmail.com>
2025-02-27 18:37:35 +0200

    Fixed name and parameter type

--- src/main/java/com/api/management/user/mapper/TaskMapper.java
+++ src/main/java/com/api/management/user/mapper/TaskMapper.java
@@ -1,11 +1,12 @@
 package com.api.management.user.mapper;
 
 import com.api.management.user.dto.task.TaskDto;
+import com.api.management.user.entity.TaskEntity;
 import com.api.management.user.mapper.base.BaseMapper;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface TaskEntity extends BaseMapper<TaskEntity, TaskDto> {
+public interface TaskMapper extends BaseMapper<TaskEntity, TaskDto> {
     @Override
     TaskDto mapEntityToDto(TaskEntity entity);
 
