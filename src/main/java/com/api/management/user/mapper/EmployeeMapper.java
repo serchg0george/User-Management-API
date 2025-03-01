53c3d1196857a5a1b2d7063f0721b9c1698b8a71
serchg0george <serchg0george@gmail.com>
2025-03-01 22:45:35 +0200

    fix: implemented custom mapper for Employee

--- src/main/java/com/api/management/user/mapper/EmployeeMapper.java
+++ src/main/java/com/api/management/user/mapper/EmployeeMapper.java
@@ -1,17 +1,71 @@
 package com.api.management.user.mapper;
 
 import com.api.management.user.dto.employee.EmployeeDto;
-import com.api.management.user.entity.EmployeeEntity;
+import com.api.management.user.entity.*;
+import com.api.management.user.exception.base.BaseNotFoundException;
 import com.api.management.user.mapper.base.BaseMapper;
-import org.mapstruct.Mapper;
+import com.api.management.user.repository.DepartmentRepository;
+import com.api.management.user.repository.PositionRepository;
+import com.api.management.user.repository.ProjectRepository;
+import com.api.management.user.repository.TimesheetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface EmployeeMapper extends BaseMapper<EmployeeEntity, EmployeeDto> {
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EmployeeMapper implements BaseMapper<EmployeeEntity, EmployeeDto> {
+
+    private final ProjectRepository projectRepository;
+    private final DepartmentRepository departmentRepository;
+    private final PositionRepository positionRepository;
+    private final TimesheetRepository timesheetRepository;
 
     @Override
-    EmployeeDto mapEntityToDto(EmployeeEntity peopleEntity);
+    public EmployeeDto mapEntityToDto(EmployeeEntity entity) {
+        return EmployeeDto.builder()
+                .id(entity.getId())
+                .firstName(entity.getFirstName())
+                .lastName(entity.getLastName())
+                .pin(entity.getPin())
+                .email(entity.getEmail())
+                .address(entity.getAddress())
+                .departmentId(entity.getDepartment().getId())
+                .positionId(entity.getPosition().getId())
+                .timeSheetEmployeeId(entity.getTimeSheetEmployee().getId())
+                .projectIds(entity.getProjects().stream()
+                        .map(ProjectEntity::getId)
+                        .toList())
+                .build();
+    }
 
     @Override
-    EmployeeEntity mapDtoToEntity(EmployeeDto peopleDto);
+    public EmployeeEntity mapDtoToEntity(EmployeeDto dto) {
+        List<ProjectEntity> projects = dto.getProjectIds().stream()
+                .map(id -> projectRepository.findById(id).orElseThrow(() -> new BaseNotFoundException(id)))
+                .toList();
+
+        DepartmentEntity department = departmentRepository.findById(dto.getDepartmentId())
+                .orElseThrow(() -> new BaseNotFoundException(dto.getDepartmentId()));
+
+        PositionEntity position = positionRepository.findById(dto.getPositionId())
+                .orElseThrow(() -> new BaseNotFoundException(dto.getPositionId()));
+
+        TimesheetEntity timesheet = timesheetRepository.findById(dto.getTimeSheetEmployeeId())
+                .orElseThrow(() -> new BaseNotFoundException(dto.getTimeSheetEmployeeId()));
+
+        return EmployeeEntity.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .pin(dto.getPin())
+                .email(dto.getEmail())
+                .address(dto.getAddress())
+                .department(department)
+                .position(position)
+                .timeSheetEmployee(timesheet)
+                .projects(projects)
+                .build();
+    }
 
 }
