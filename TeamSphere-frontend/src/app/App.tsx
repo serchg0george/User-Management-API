7e534ddad63a210f63cce075a6fa50a241f54811
serchg0george <serchg0george@gmail.com>
2025-03-05 19:48:26 +0200

    refactor: moved, using alias for imports, changed route name

--- TeamSphere-frontend/src/app/App.tsx
+++ TeamSphere-frontend/src/app/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import './App.css';
+import '@/app/App.css';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import Company from '../src/components/forms/Company';
-import Auth from '../src/components/forms/AuthForm';
-import Navigation from './components/forms/Navigation.tsx';
-import Role from "./components/forms/Role.tsx";
-import Department from "./components/forms/Department.tsx";
-import Position from "./components/forms/Position.tsx";
-import Project from "./components/forms/Project.tsx";
-import Timesheet from "./components/forms/Timesheet.tsx";
-import Employee from "./components/forms/Employee.tsx";
+import Company from '@/components/forms/Company.tsx';
+import Auth from '@/components/forms/AuthForm.tsx';
+import Navigation from '../components/forms/Navigation.tsx';
+import Role from "@/components/forms/Role.tsx";
+import Department from "@/components/forms/Department.tsx";
+import Position from "@/components/forms/Position.tsx";
+import Project from "@/components/forms/Project.tsx";
+import Timesheet from "@/components/forms/Timesheet.tsx";
+import Employee from "@/components/forms/Employee.tsx";
 
 const App: React.FC = () => {
     return (
@@ -17,7 +17,7 @@ const App: React.FC = () => {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Auth/>}/>
-                    <Route path="/main" element={<Navigation/>}/>
+                    <Route path="/navigation" element={<Navigation/>}/>
                     <Route path="/auth" element={<Auth/>}/>
                     <Route path="/company" element={<Company/>}/>
                     <Route path="/role" element={<Role/>}/>
@@ -26,7 +26,7 @@ const App: React.FC = () => {
                     <Route path="/project" element={<Project/>}/>
                     <Route path="/timesheet" element={<Timesheet/>}/>
                     <Route path="/employee" element={<Employee/>}/>
-                    <Route path="*" element={<Navigate to="/main"/>}/>
+                    <Route path="*" element={<Navigate to="/navigation"/>}/>
                 </Routes>
             </div>
         </Router>
