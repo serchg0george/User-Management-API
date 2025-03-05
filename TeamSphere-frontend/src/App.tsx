26e35cf5c54019f52aa4db7a878ff81dc0f6fd78
serchg0george <serchg0george@gmail.com>
2025-03-05 17:13:58 +0200

    refactor: added routing
    Added routing for project, timesheet, employee components

--- TeamSphere-frontend/src/App.tsx
+++ TeamSphere-frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Company from '../src/components/forms/Company';
 import Auth from '../src/components/forms/AuthForm';
 import Navigation from './components/forms/Navigation.tsx';
 import Role from "./components/forms/Role.tsx";
 import Department from "./components/forms/Department.tsx";
 import Position from "./components/forms/Position.tsx";
+import Project from "./components/forms/Project.tsx";
+import Timesheet from "./components/forms/Timesheet.tsx";
+import Employee from "./components/forms/Employee.tsx";
 
 const App: React.FC = () => {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Auth />} />
-                    <Route path="/main" element={<Navigation />} />
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="/company" element={<Company />} />
-                    <Route path="/role" element={<Role />} />
-                    <Route path="/department" element={<Department />} />
-                    <Route path="/position" element={<Position />} />
-                    <Route path="*" element={<Navigate to="/main" />} />
+                    <Route path="/" element={<Auth/>}/>
+                    <Route path="/main" element={<Navigation/>}/>
+                    <Route path="/auth" element={<Auth/>}/>
+                    <Route path="/company" element={<Company/>}/>
+                    <Route path="/role" element={<Role/>}/>
+                    <Route path="/department" element={<Department/>}/>
+                    <Route path="/position" element={<Position/>}/>
+                    <Route path="/project" element={<Project/>}/>
+                    <Route path="/timesheet" element={<Timesheet/>}/>
+                    <Route path="/employee" element={<Employee/>}/>
+                    <Route path="*" element={<Navigate to="/main"/>}/>
                 </Routes>
             </div>
         </Router>
