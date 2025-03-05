2cc8108f6a50ece091faaae2936ddd5564d8ad30
serchg0george <serchg0george@gmail.com>
2025-03-05 11:21:31 +0200

    feat: implemented registration and login feature

---
+++ TeamSphere-frontend/src/components/forms/AuthForm.tsx
@@ -0,0 +1,86 @@
+import {useState} from 'react';
+import axios from 'axios';
+import {useForm, SubmitHandler} from 'react-hook-form';
+import {useNavigate} from 'react-router-dom';
+import {AuthFormData} from "../models/authFormData.ts";
+import {AuthResponse} from "../models/authResponseData.ts";
+
+const AuthForm = () => {
+    const [isRegister, setIsRegister] = useState<boolean>(true);
+    const {register, handleSubmit, formState: {errors}} = useForm<AuthFormData>({
+        defaultValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+        },
+    });
+    const navigate = useNavigate();
+
+    const onSubmit: SubmitHandler<AuthFormData> = async (data) => {
+
+        const baseUrl: string = 'http://localhost:8080/';
+
+        const url = isRegister
+            ? baseUrl + 'api/v1/auth/register'
+            : baseUrl + 'api/v1/auth/authenticate';
+
+        try {
+            const response = await axios.post<AuthResponse>(url, data);
+            console.log('Response:', response.data);
+            localStorage.setItem('token', response.data.token); // Store the token in localStorage
+            navigate('/company'); // Redirect to the Company component
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.error('Error:', error.response?.data || error.message);
+            } else {
+                console.error('Unexpected error:', error);
+            }
+        }
+    };
+
+    return (
+        <div>
+            <h1>{isRegister ? 'Register' : 'Login'}</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {isRegister && (
+                    <>
+                        <input
+                            {...register('firstName', {required: 'First name is required'})}
+                            placeholder="First Name"
+                        />
+                        {errors.firstName && <p>{errors.firstName.message}</p>}
+                        <input
+                            {...register('lastName', {required: 'Last name is required'})}
+                            placeholder="Last Name"
+                        />
+                        {errors.lastName && <p>{errors.lastName.message}</p>}
+                    </>
+                )}
+                <input
+                    {...register('email', {
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^\S+@\S+$/i,
+                            message: 'Invalid email address'
+                        }
+                    })}
+                    placeholder="Email"
+                />
+                {errors.email && <p>{errors.email.message}</p>}
+                <input
+                    {...register('password', {required: 'Password is required'})}
+                    type="password"
+                    placeholder="Password"
+                />
+                {errors.password && <p>{errors.password.message}</p>}
+                <button type="submit">{isRegister ? 'Register' : 'Login'}</button>
+            </form>
+            <button onClick={() => setIsRegister(!isRegister)}>
+                {isRegister ? 'Already have an account? Login' : 'Need an account? Register'}
+            </button>
+        </div>
+    );
+};
+
+export default AuthForm;
---
+++ TeamSphere-frontend/src/components/models/authFormData.ts
@@ -0,0 +1,6 @@
+export interface AuthFormData {
+    firstName?: string; // Optional for login
+    lastName?: string;  // Optional for login
+    email: string;
+    password: string;
+}
