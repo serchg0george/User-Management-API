1e987570c4cc014aa3a8f7aaeb427ba875245f27
serchg0george <serchg0george@gmail.com>
2025-03-05 14:41:36 +0200

    refactor: added routing for new components

--- TeamSphere-frontend/src/App.tsx
+++ TeamSphere-frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Company from '../src/components/forms/Company';
-import './App.css';
 import Auth from '../src/components/forms/AuthForm';
+import Navigation from './components/forms/Navigation.tsx';
+import Role from "./components/forms/Role.tsx";
+import Department from "./components/forms/Department.tsx";
+import Position from "./components/forms/Position.tsx";
 
 const App: React.FC = () => {
     return (
@@ -10,8 +14,12 @@ const App: React.FC = () => {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Auth />} />
+                    <Route path="/main" element={<Navigation />} />
                     <Route path="/auth" element={<Auth />} />
                     <Route path="/company" element={<Company />} />
+                    <Route path="/role" element={<Role />} />
+                    <Route path="/depratment" element={<Department />} />
+                    <Route path="/position" element={<Position />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </div>
