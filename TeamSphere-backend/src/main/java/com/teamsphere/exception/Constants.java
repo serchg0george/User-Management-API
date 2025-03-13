57010466ed1f423b635c9b53af3c2b143718f212
Serhii Zavada <serchg0george@gmail.com>
2025-03-13 11:06:40 +0200

    refactor: removed unnecessary files

--- TeamSphere-backend/src/main/java/com/teamsphere/exception/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.teamsphere.exception;
-
-public interface Constants {
-    String CREATE_SUCCESS = "Created successfully!";
-    String UPDATE_SUCCESS = "Updated successfully!";
-    String DELETE_SUCCESS = "Deleted successfully!";
-}
